9a512bc199e2fb3a41329de3f62b6df28441183e
andrea <andreatchirillano@hotmail.com>
2023-10-16 18:33:54 -0300

    Add: loggin

--- shirt-store-frontend/src/app/app.module.ts
+++ shirt-store-frontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CatalogueComponent } from './catalogue/catalogue.component';
+import { LogginComponent } from './loggin/loggin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    CatalogueComponent
+    CatalogueComponent,
+    LogginComponent
   ],
   imports: [
     BrowserModule,
