ef85ea8350cf334f783cac266652587f627a2a70
Andrea Chirillano <andreatchirillano@hotmail.com>
2024-01-31 12:54:17 -0300

    First commit backend Java Spring

---
+++ shirt-store-backend/src/main/java/com/shirtstore/controller/controller.java
@@ -0,0 +1,5 @@
+package com.shirtstore.controller;
+
+public class controller {
+    
+}
---
+++ shirt-store-backend/src/test/java/com/shirtstore/shirtstore/ShirtStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shirtstore.shirtstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShirtStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
